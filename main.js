964426fda3543892f43de7b2471065d6a6970a04
Girish Babu <girish.babu@saaslabs.co>
2023-09-21 19:53:11 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+// var polygon = [
+// { height: 10, width: 30 },
+// { height: 20, width: 90 },
+// { height: 54, width: 32 }
+// ];
+var areas = []
+let polygonFn = (polygon) => {
+    polygon.forEach((element) => {
+        let calcArea = element.height * element.width
+        // console.log(calcArea)
+        areas.push(calcArea)
+    });
+    return areas
+}
+// polygonFn([{ height: 10, width: 30 }, { height: 20, width: 90 }, { height: 54, width: 32 }])
+
+// var paints = [ { color: 'red' }, { color: 'blue' }, { color: 'yellow' }];        
+let colors = []
+let pick = (paints, col) => {
+    paints.map((a) => {
+        if (a[col]) {
+            // console.log(a[col])
+            colors.push(a[col])
+        }
+    })
+    return colors
+}
+
+// pick([{ color: 'red' }, { color: 'blue' }, { color: 'yellow' }, { colors: 'violet' }], 'color')
---
+++ readme.md
@@ -0,0 +1,8 @@
+<h5>Polygon and Pick functions using forEach and map</h5>
+<p>Open the console and try the function</p>
+<ol>
+    <li class="pb-2"><code>polygonFn([{ height: 10, width: 30 }, { height: 20, width: 90 }, { height: 54, width: 32 }])</code>
+    </li>
+    <li><code>pick([{ color: 'red' }, { color: 'blue' }, { color: 'yellow' }, { colors: 'violet' }], 'color')</code>
+    </li>
+</ol>
